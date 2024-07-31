c743f84dff248700952fa560b3b2ec862ab58806
wen-3 <sd63324@gmail.com>
2024-07-31 19:51:25 +0800

    push

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/vite-sample/',
   plugins: [
     vue(),
     vueDevTools(),
