b7195b33a6f209f6f3db52b7817efbc8577b13ac
wen-3 <sd63324@gmail.com>
2024-07-31 16:44:48 +0800

    push

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+
+const num = ref(0);
+function clickMe() {
+  num.value ++;
+}
+</script>
+
+<template>
+  <div>
+    {{ num }}
+    <button type="button" v-on:click="clickMe">點我</button>
+  </div>
+</template>
